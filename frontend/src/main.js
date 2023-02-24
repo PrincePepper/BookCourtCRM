70f5f0cd6c2c686596be46b88b242a6bacecc7bc
semyo <Semyon3336@gmail.com>
2023-02-25 05:28:03 +1000

    awee

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -7,7 +7,7 @@ import store from './store/index'
 import 'materialize-css/dist/js/materialize.min'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -21,8 +21,28 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primeicons/primeicons.css'
 
 const vuetify = createVuetify({
-  components,
-  directives,
+    components,
+    directives,
 })
 
 createApp(App).use(store).use(router).use(vuetify).use(PrimeVue).component('Menubar', Menubar).component('InputText', InputText).component('FileUpload', FileUpload).component('Button', Button).mount('#app')
+
+
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import {BootstrapVue} from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+
+
+Vue.config.productionTip = false
+Vue.use(Vuelidate)
+Vue.use(BootstrapVue)
+Vue.use(store)
+Vue.use(VueRouter)
+Vue.use(router)
+
+new Vue({
+    render: h => h(App),
+    store,
+    router
+}).$mount('#app')
